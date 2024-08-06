e2033419dfbfce06bb490d314903b58609a64f9c
Ezekiel Asigbey <ezasigbey@gmail.com>
2024-08-06 00:56:45 +0000

    Day 5

--- August/myapp/views.py
+++ August/myapp/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from .forms import BookModelForm
 
-def index(request):
-    context = {
-        'name': 'World',  # This is the dynamic data we'll pass to the template
-    }
-    return render(request, 'index.html', context)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html', {'title': form.cleaned_data['title']})
+    else:
+        form = BookModelForm()
+
+    return render(request, 'add_book.html', {'form': form})
